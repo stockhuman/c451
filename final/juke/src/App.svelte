c60a6780e0b6b62f8c091240f3d2f8f79dede8d4
Michael Hemingway <mpghemingway@gmail.com>
2023-11-02 13:55:22 -0400

    Changed my mind: build with vite & svelte instead

---
+++ final/juke/src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Canvas from './lib/Canvas.svelte'
+</script>
+
+<main>
+  <Canvas />
+</main>
---
+++ final/juke/src/lib/Canvas.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+</script>
+
+<div>
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  div {
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
