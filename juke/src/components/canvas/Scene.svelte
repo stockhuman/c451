6894225e3400fc1dc5bc7e021e723f540236411c
Michael Hemingway <mpghemingway@gmail.com>
2023-11-04 19:55:49 -0400

    POC disc

--- juke/src/components/canvas/Scene.svelte
+++ juke/src/components/canvas/Scene.svelte
@@ -3,9 +3,9 @@
   import { Environment, OrbitControls, interactivity, useGltf } from '@threlte/extras'
   import { spring } from 'svelte/motion'
   import { MeshStandardMaterial } from 'three/src/materials/MeshStandardMaterial.js'
-  import { builder } from '../../functions/builder'
   import blankDisc from '../../assets/blank-disc.glb'
   import envmap from '../../assets/empty_workshop_1k.hdr'
+  import { generateTitle, generateTrack } from '../../functions/builder'
 
   // recolor the disc
   const mat = new MeshStandardMaterial({ color: 0xad0d10, roughness: 0.5 })
@@ -17,14 +17,23 @@
       if (o.isMesh) o.material = mat
     })
   }
-  const pattern = builder()
+  const gen = Array(75)
+  for (let i = 0; i < gen.length; i++) {
+    let a = Array(22).fill('1')
+    a.map((_, s) => Math.random() > 0.5 ? a[s] = '1' : a[s] = '0')
+    gen[i] = a.join('')
+  }
+  const trackA = { title: 'Garbanzo bob', data: gen, type: 'original', side: 'A' }
+  // @ts-ignore
+  const pattern = generateTrack(trackA)
+  const title = generateTitle(trackA.title)
 
   interactivity()
   const scale = spring(1)
   let rotation = 0
-  // useFrame((_, delta) => {
-  //   rotation += delta / 2
-  // })
+  useFrame((_, delta) => {
+    rotation -= delta / 2
+  })
 </script>
 
 <Environment files={envmap} format="hdr" />
@@ -46,8 +55,7 @@
     <T.Mesh>
       <T is={$blank.nodes['Scene']} scale={100} />
     </T.Mesh>
-    {#await pattern then pat}
-      <T.Mesh geometry={pat} material={mat2} />
-    {/await}
+    <T.Mesh geometry={pattern} material={mat2} />
+    <T.Mesh geometry={title} material={mat2} />
   </T.Group>
 {/if}
--- juke/src/functions/builder.ts
+++ juke/src/functions/builder.ts
@@ -6,23 +6,47 @@ import jetbrainsMono from '../assets/JetBrainsMono.json'
 
 type RecordSide = {
   title: string
-  data: DataPoint[]
-  /**
-   * - `original` describes the toy's 75-note limit across all groves, to ease in song composition
-   * - `packed` instead maximizes available note density in the outer grooves, to a maximum of 156
-   */
-  mode: 'original' | 'packed'
-} | null
+  side: 'A' | 'B'
+} & (
+  | {
+      /**
+       * Describes the toy's original 75-note limit across all groves, to ease in song composition
+       * From innermost to outer, 1 representing a notch, 0 nothing
+       * ex: "0100101010010010101011"
+       */
+      type: 'original'
+      data: string[]
+    }
+  | {
+      /**
+       * Like packed, except data is laid out automatically.
+       * All the data is sequentially placed in one string.
+       * ex.: "0101010001010100100111101011011..."
+       */
+      type: 'raw'
+      data: string
+    }
+)
 
-type DataPoint = {}
+export function generateTrack(record: RecordSide) {
+  const radius = 2.85 // the radius of the innermost track
+  const notches: BufferGeometry[] = []
 
-/**
- * Builds A record given a set of note positions
- */
-export async function builder(sideA?: RecordSide, sideB?: RecordSide): Promise<BufferGeometry> {
-  const titleA = generateTitle(sideA?.title.toUpperCase() || '1234567 890OD')
+  if (record.type === 'original') {
+    for (let i = 0; i < record.data.length; i++) {
+      const interval = record.data[i]
+      console.log(interval)
+      for (let t = 0; t < interval.length; t++) {
+        if (interval[t] === '0') continue
+        const notch = new BoxGeometry(0.1, 0.2, 0.1)
+        notch.translate(radius + t * 0.1407, 0.14, 0)
+        notch.rotateY(deg2rad((360 / 75) * i))
+        notches.push(notch)
+      }
+    }
+  }
 
-  const geometry = BufferGeometryUtils.mergeGeometries([titleA])
+  const geometry = BufferGeometryUtils.mergeGeometries(notches)
   return geometry
 }
 
@@ -31,46 +55,33 @@ const font = loader.parse(jetbrainsMono)
 
 /**
  * Draws a given title
- * @TODO split title into individual letters, arrange by title length
  * @TODO make long titles split into top and bottom halves? Make config option?
  */
-function generateTitle(title: string): BufferGeometry {
+export function generateTitle(title: string): BufferGeometry {
+  const string = (title || 'untitled').normalize('NFD').toUpperCase()
   const text: BufferGeometry[] = []
-  // const isEven = title.length % 2 === 0
-  // if (!isEven) {
-  //   // single middle letter
-
-  // }
-  // for (let i = 0; i < title.length; i++) {
-  //   const letter = new TextGeometry(title[i], {
-  //     font: font,
-  //     size: 0.4,
-  //     height: 0.07,
-  //     curveSegments: 12,
-  //   })
-  //   // offset text based on text length (z-up, only x+y)
-  //   letter.translate(-1.5, 1, 0.1)
-  //   text.push(letter)
-  // }
-
-  const charRads = Math.PI / 20
+  const charRads = Math.PI / 18
   const radius = 1.45
 
-  for (let i = 0; i < title.length; i++) {
-    if (title[i] === ' ') continue
-    const letter = new TextGeometry(title[i], {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') continue
+    const letter = new TextGeometry(string[i], {
       font: font,
-      size: 0.3,
-      height: 0.07,
+      size: 0.33,
+      height: 0.1,
       curveSegments: 12,
     })
     letter.rotateZ(Math.PI - (i * charRads + 0.1))
     letter.translate(radius * -Math.sin(i * charRads), radius * -Math.cos(i * charRads), 0.1)
-    
+
     text.push(letter)
   }
 
-  const geometry = BufferGeometryUtils.mergeGeometries(text).rotateX(deg2rad(-90))
+  const geometry = BufferGeometryUtils.mergeGeometries(text)
+    // flatten text against surface
+    .rotateX(deg2rad(-90))
+    // centers the text with the topmost peg hole, to match original
+    .rotateY((charRads * string.length) / 2)
   return geometry
 }
 
