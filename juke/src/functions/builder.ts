a3db966be1c352d405c5e07ec81543830bb18a38
Michael Hemingway <mpghemingway@gmail.com>
2023-11-21 16:26:23 -0500

    Use hexadecimal for encoding, simplify types

--- juke/src/functions/builder.ts
+++ juke/src/functions/builder.ts
@@ -2,46 +2,34 @@
 import { BufferGeometry, BoxGeometry, BufferGeometryUtils } from '../three'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-import jetbrainsMono from '../assets/JetBrainsMono.json'
+import typeface from '../assets/IBMPlexMonoSemiBold.json'
 
-type RecordSide = {
+export type RecordSide = {
   title: string
   side: 'A' | 'B'
-} & (
-  | {
-      /**
-       * Describes the toy's original 75-note limit across all groves, to ease in song composition
-       * From innermost to outer, 1 representing a notch, 0 nothing
-       * ex: "0100101010010010101011"
-       */
-      type: 'original'
-      data: string[]
-    }
-  | {
-      /**
-       * Like packed, except data is laid out automatically.
-       * All the data is sequentially placed in one string.
-       * ex.: "0101010001010100100111101011011..."
-       */
-      type: 'raw'
-      data: string
-    }
-)
+  /**
+   * Describes the toy's original 75-note limit across all groves, to ease in song composition
+   * From innermost to outer, 1 representing a notch, 0 nothing
+   * ex: "0100101010010010101011"
+   * 
+   * Note that it is stored as a hex string, so it must be decompressed before use
+   */
+  data: string[]
+}
 
 export function generateTrack(record: RecordSide) {
   const radius = 2.85 // the radius of the innermost track
   const notches: BufferGeometry[] = []
 
-  if (record.type === 'original') {
-    for (let i = 0; i < record.data.length; i++) {
-      const interval = record.data[i]
-      for (let t = 0; t < interval.length; t++) {
-        if (interval[t] === '0') continue
-        const notch = new BoxGeometry(0.1, 0.2, 0.1)
-        notch.translate(radius + t * 0.1407, 0.14, 0)
-        notch.rotateY(deg2rad((360 / 75) * i))
-        notches.push(notch)
-      }
+  for (let i = 0; i < record.data.length; i++) {
+    const interval = decompress(record.data[i])
+    // Hex string encodings allow for 24 bits of data, but the original toy only had 22 notches
+    for (let t = 0; t < 22; t++) {
+      if (interval[t] === '0') continue
+      const notch = new BoxGeometry(0.08, 0.2, 0.08)
+      notch.translate(radius + t * 0.1407, 0.14, 0)
+      notch.rotateY(deg2rad((360 / 75) * i))
+      notches.push(notch)
     }
   }
 
@@ -50,7 +38,7 @@ export function generateTrack(record: RecordSide) {
 }
 
 const loader = new FontLoader()
-const font = loader.parse(jetbrainsMono)
+const font = loader.parse(typeface)
 
 /**
  * Draws a given title
@@ -59,14 +47,14 @@ const font = loader.parse(jetbrainsMono)
 export function generateTitle(title: string): BufferGeometry {
   const string = (title || 'untitled').normalize('NFD').toUpperCase()
   const text: BufferGeometry[] = []
-  const charRads = Math.PI / 18
+  const charRads = Math.PI / 15
   const radius = 1.45
 
   for (let i = 0; i < string.length; i++) {
     if (string[i] === ' ') continue
     const letter = new TextGeometry(string[i], {
       font: font,
-      size: 0.33,
+      size: 0.35,
       height: 0.1,
       curveSegments: 12,
     })
@@ -86,4 +74,15 @@ export function generateTitle(title: string): BufferGeometry {
 
 function deg2rad(degrees: number) {
   return degrees * (Math.PI / 180)
+}
+
+/**
+ * Decompresses a hex string into a binary string
+ * @param data a hex string
+ */
+function decompress(data: string) {
+  const binaryString = Number('0x' + data)
+    .toString(2)
+    .padStart(data.length * 4, '0')
+  return binaryString
 }
