38a5951338e64767faf2356a0541f9668c7739db
Michael Hemingway <mpghemingway@gmail.com>
2023-12-05 14:46:39 -0500

    Update Scene.svelte

--- juke/src/components/canvas/Scene.svelte
+++ juke/src/components/canvas/Scene.svelte
@@ -6,8 +6,9 @@
   import envmap from '../../assets/empty_workshop_1k.hdr'
   import { generateTitle, generateTrack, type RecordSide } from '../../functions/builder'
   import { scene as global, disc } from '../../functions/store'
-  import type { BufferGeometry } from 'three'
 
+  // @ts-ignore
+  let disq = null
   // recolor the disc
   const mat = new MeshStandardMaterial({ color: 0x286bb5, roughness: 0.5 })
   const mat2 = new MeshStandardMaterial({ color: 0x164477, roughness: 0.3 })
@@ -20,22 +21,21 @@
     })
     global.set(scene)
   }
-  let patternA: RecordSide, patternB: RecordSide, titleA: BufferGeometry, titleB: BufferGeometry
+  // @ts-ignore
+  $: patternA = disq.a ? (generateTrack(disq.a) as unknown as RecordSide) : null
+  // @ts-ignore
+  $: patternB = disq.b ? (generateTrack(disq.b) as unknown as RecordSide) : null
+  // @ts-ignore
+  $: titleA = disq.a ? generateTitle(disq.a.title) : generateTitle('No disc loaded')
+  // @ts-ignore
+  $: titleB = disq.b ? generateTitle(disq.b.title) : generateTitle('No disc loaded')
+
+  // let patternA: RecordSide, patternB: RecordSide, titleA: BufferGeometry, titleB: BufferGeometry
 
   disc.subscribe(d => {
-    console.log(d)
-    if (d?.a) {
-      patternA = generateTrack(d.a) as unknown as RecordSide
-      titleA = generateTitle(d.a.title)
-    } else {
-      titleA = generateTitle('No disc loaded')
-    }
-    if (d?.b) {
-      patternB = generateTrack(d.b) as unknown as RecordSide
-      titleB = generateTitle(d.b.title)
-    } else {
-      titleB = generateTitle('No disc loaded')
-    }
+    if (!d) return
+    disq = d
+    global.set(scene)
   })
 
   let rotation = 0
@@ -59,18 +59,22 @@
       <T is={$blank.nodes['Scene']} />
     </T.Mesh>
     {#if patternA}<T.Mesh geometry={patternA} material={mat2} name="groves-A" />{/if}
-    <T.Mesh geometry={titleA} material={mat2} name="title-A" />
-    {#if patternB}<T.Mesh
+    {#if titleA}<T.Mesh geometry={titleA} material={mat2} name="title-A" />{/if}
+    {#if patternB}
+      <T.Mesh
         geometry={patternB}
         material={mat2}
         name="groves-B"
         position={[0, -0.0019, 0]}
-        rotation={[0, 0, Math.PI]} />{/if}
-    <T.Mesh
-      geometry={titleB}
-      material={mat2}
-      name="title-B"
-      position={[0, -0.0019, 0]}
-      rotation={[0, 0, Math.PI]} />
+        rotation={[0, 0, Math.PI]} />
+    {/if}
+    {#if titleB}
+      <T.Mesh
+        geometry={titleB}
+        material={mat2}
+        name="title-B"
+        position={[0, -0.0019, 0]}
+        rotation={[0, 0, Math.PI]} />
+    {/if}
   </T.Group>
 {/if}
