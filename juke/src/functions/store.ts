612b9a26e46982a1ff3578c074fef452a04684c4
Michael Hemingway <mpghemingway@gmail.com>
2023-12-05 12:33:49 -0500

    Record -> Disc
    Record is a TS default

--- juke/src/functions/store.ts
+++ juke/src/functions/store.ts
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store'
 import type { Object3D } from 'three'
 import type { RecordSide } from './builder'
 
-type Record = {
+export type Disc = {
   a: RecordSide | null
   b: RecordSide | null
   meta: {
@@ -13,7 +13,7 @@ type Record = {
 }
 
 export const scene = writable<Object3D>()
-export const disc = writable<Record>({
+export const disc = writable<Disc>({
   a: null,
   b: null,
   meta: {
